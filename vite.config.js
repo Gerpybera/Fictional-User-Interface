8a0fc28a5976393e422a1a20a63b4db0a8d7a65c
Gerpybera <malcolmsemedo333@gmail.com>
2025-12-05 14:25:37 +0100

    upgrade file paths (i guess?)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/Fictional-User-Interface/',
-})
+  base: "/Fictional-User-Interface/",
+});
