19d8b42f0293f8d773c213021b9a4bfd48f5010b
Gerpybera <malcolmsemedo333@gmail.com>
2025-12-06 14:14:02 +0100

    making the width and height responsive

--- src/health.js
+++ src/health.js
@@ -1,8 +1,8 @@
 export default class Health {
   constructor(x, y, ctx, bColor, textUp, textUn, index) {
     // Design PART
-    this.width = 350;
-    this.height = 110;
+    this.width = ctx.canvas.width / 6;
+    this.height = ctx.canvas.height * 0.11;
     this.x = x;
     this.y = y;
     this.textUpper = textUp;
